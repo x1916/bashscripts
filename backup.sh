a4611bec62aaf816b24fd80a0364de574843c383
x1916 <x19164131@student.ncirl.ie>
2020-10-20 00:39:25 +0100

    Updated Tue 20 Oct 2020 00:39:25 IST

--- backup.sh
+++ backup.sh
@@ -3,4 +3,24 @@
 # Script to perform backup of file system.
 # Exludes the backup tarball and the one-file-system flag directories.
 
-sudo tar -cvpzf backup.tar.gz --exclude=/backup.tar.gz --one-file-system / 
+read -r -p "Create new Cron backup job? (y/N)?" input
+
+case $input in
+	[yY][eE][sS]|[yY])
+
+	cat <(crontab -l) <(echo "0 0 * * * backup.sh") | crontab -
+break
+;;
+	[nN][oO]|[nN])
+break
+;;
+	*)
+echo "Invalid input..."
+;;
+esac
+
+
+sudo tar -cvpzf $(date -I)-backup.tar.gz --exclude=/*-backup.tar.gz --one-file-system / 
+
+find backup/ -type f -iname "*-backup.tar.gz" -mtime +5 -delete
+
